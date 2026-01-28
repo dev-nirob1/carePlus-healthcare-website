5bcfe2f020bcd562257b35ff38684b8bdcb1f419
dev-nirob1 <dev.nirob1@gmail.com>
2026-01-29 00:11:22 +0600

    doctors page

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,8 +1,21 @@
+import React, { useEffect } from "react";
+import PageHero from "../components/common/PageHero";
+import DoctorsGrid from "../components/doctors/DoctorsGrid";
+
 function Doctors() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    document.title = "Our Doctors | CarePlus Clinic";
+  }, []);
+
   return (
     <div className="doctors-page">
-      <h1>Our Doctors</h1>
-      <p>Meet our healthcare professionals</p>
+      <PageHero
+        title="Meet Our Specialists"
+        bgImage="https://images.unsplash.com/photo-1559839734-2b71ea197ec2?q=80&w=2600&auto=format&fit=crop"
+        pageTitle="Our Doctors"
+      />
+      <DoctorsGrid />
     </div>
   );
 }
